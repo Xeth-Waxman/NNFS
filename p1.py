2d1c028ff01c5a86c54de2126f58249a737be732
Xeth Waxman <xwaxman@gmail.com>
2021-01-12 21:03:37 -0500

    Update p1.py
    Moved from basic library validation to creating the input/output scheme of a basic neuron

--- p1.py
+++ p1.py
@@ -1,7 +1,6 @@
-import sys
-import numpy as np
-import matplotlib
+inputs = [1.1, 2.2, 3.3]
+weights = [6.6, 5.5, 4.4]
+bias = 3
 
-print("python: ", sys.version)
-print("Numpy: ", np.__version__)
-print("Matplotlib: ", matplotlib.__version__)
+output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias
+print("Output: ", output)
